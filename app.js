ef5f5b5f9f1df3bd12b7bd57f18dc343c75d85f9
Илья Андреев <i.andreyev@syberry.com>
2021-03-02 13:37:50 +0300

    CORS fix

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const routes = require('./routes/routes')
 const app = express()
 const port = 3001
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+app.use(cors())
 
 routes(app)
 
